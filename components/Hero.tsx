c66118ea477f8bec105938e36ffbdca5b5adcb1a
chysomm <chisommuorah@gmail.com>
2026-02-12 20:41:05 +0100

    hero update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,20 +1,105 @@
+"use client";
+
+import Image from "next/image";
 import ResumeButton from "./ui/Resumebutton";
+import { motion, Variants } from "framer-motion";
 
 export default function Hero() {
-  return (
-    <section className="min-h-[60vh] sm:min-h-[75vh] flex items-start lg:items-center container mx-auto p-5 lg:px-14 max-w-7xl flex-col lg:flex-row gap-10 mt-36 lg:mt-14">
-      <div className="flex flex-col gap-7">
-        <h1 className="text-5xl lg:text-7xl font-bold text-gradient ">
-          Chisom Muorah
-        </h1>
-        <p className="text-lg text-accent max-w-xl">
-          Building amazing web experiences
-        </p>
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
 
-        <div className="flex items-center gap-10">
-          <ResumeButton />
-        </div>
-      </div>
+  const fadeUp: Variants = {
+    hidden: { opacity: 0, y: 40 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 5,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const imageReveal: Variants = {
+    hidden: { opacity: 0, scale: 0.85 },
+    show: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  };
+
+  return (
+    <section className="min-h-[70vh] container mx-auto px-5 lg:px-14 max-w-7xl mt-24 lg:mt-36 flex">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="grid lg:grid-cols-2 items-center gap-12 w-full">
+        {/* LEFT SIDE */}
+        <motion.div variants={fadeUp} className="flex flex-col gap-7">
+          <motion.h1
+            variants={fadeUp}
+            className="text-4xl sm:text-5xl lg:text-7xl font-bold text-gradient">
+            Chisom Muorah
+          </motion.h1>
+          <motion.p variants={fadeUp} className="text-lg text-accent max-w-xl">
+            Building amazing web experiences
+          </motion.p>
+          <motion.div variants={fadeUp} className="flex items-center gap-10">
+            <ResumeButton />
+          </motion.div>
+        </motion.div>
+
+        {/* RIGHT SIDE */}
+
+        <motion.div variants={imageReveal} className="relative aspect-570/650">
+          <Image
+            src="https://res.cloudinary.com/dfyo6umic/image/upload/f_auto,q_auto/v1770904189/profile_image.png"
+            alt="profile image"
+            fill
+            className="object-cover rounded-sm shadow-2xl shadow-black/30 animate-[float_6s_ease-in-out_infinite] z-10"
+            priority
+          />
+
+          <div className="absolute bg-accent-gradient opacity-10 w-full h-full z-20"></div>
+
+          <motion.div
+            animate={{ rotate: [-2, -1.8, -2] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -top-0.5 -left-0.5 -right-0.5 -bottom-0.5 border-2 rounded-lg border-[#779ccf] -rotate-2 bg-[#64ff22]/30 scale-[0.99]"></motion.div>
+          <motion.div
+            animate={{ rotate: [2, 2.2, 2] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -top-0.5 -left-0.5 -right-0.5 -bottom-0.5 border-2 rounded-lg border-[#08a5ff] rotate-2 bg-[#097a9e]/30 scale-[0.99]"></motion.div>
+
+          <motion.div
+            animate={{ rotate: [15, -15, 15] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -top-5 -right-5 h-16 w-16 border rounded-lg border-[#08a5ff] rotate-15 bg-[#097a9e]/20 z-10"></motion.div>
+          <motion.div
+            animate={{ rotate: [-15, 15, -15] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -left-5 -bottom-5 border h-16 w-16 rounded-lg border-[#779ccf] -rotate-15 z-10 bg-[#64ff22]/20"></motion.div>
+          <motion.div
+            animate={{ scale: [1, 0.98, 1] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -top-5 -left-5 h-16 w-16 border rounded-lg border-[#08a5ff] rotate-15 bg-[#08a5ff]/50"></motion.div>
+          <motion.div
+            animate={{ scale: [1, 0.98, 1] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="absolute -right-5 -bottom-5 border h-16 w-16 rounded-lg border-[#779ccf] -rotate-15 bg-[#779ccf]/50"></motion.div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
--- components/ui/Resumebutton.tsx
+++ components/ui/Resumebutton.tsx
@@ -11,7 +11,7 @@ const ResumeButton: React.FC<ResumeButtonProps> = ({ className = "" }) => {
     <a
       href="/Chisom_Muorah_Resume.pdf"
       download="Chisom_Muorah_Resume.pdf"
-      className={`relative inline-flex items-center justify-center bg-background px-5 py-2 rounded-sm font-medium transition-all duration-300 ease-out shadow-[10px_10px_20px_rgba(150,150,150,0.25),-10px_-10px_20px_rgba(150,150,150,0.25)] hover:shadow-[inset_6px_6px_200px_gba(150,150,150,0.25), inset_-6px_-6px_20px_rgba(150,150,150,0.45)] hover:scale-[1.02] active:scale-[0.98] hover:bg-accent-gradient ${className}
+      className={`relative inline-flex items-center justify-center bg-background px-5 py-2 rounded-sm text-sm md:text-base lg:text-lg font-medium transition-all duration-300 ease-out shadow-[10px_10px_20px_rgba(150,150,150,0.25),-10px_-10px_20px_rgba(150,150,150,0.25)] hover:shadow-[inset_6px_6px_200px_gba(150,150,150,0.25), inset_-6px_-6px_20px_rgba(150,150,150,0.45)] hover:scale-[1.02] active:scale-[0.98] hover:bg-accent-gradient ${className}
       `}>
       Download Resume
     </a>
