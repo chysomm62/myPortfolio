36f01fb71e21429d975ea5b4ad8718b9f70270f3
chysomm <chisommuorah@gmail.com>
2026-02-19 21:06:34 +0100

    update nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,10 +4,16 @@ import { useEffect, useState } from "react";
 import { SunIcon, MoonIcon, MenuIcon, CloseIcon } from "./icons";
 import { NAV_LINKS } from "../lib/navLinks";
 import ResumeButton from "./ui/Resumebutton";
+import router from "next/router";
 
 export default function Nav() {
   const [open, setOpen] = useState(false);
   const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  const linkClick = (link: string) => {
+    setOpen(false);
+    router.push(link);
+  };
 
   useEffect(() => {
     try {
@@ -52,30 +58,34 @@ export default function Nav() {
 
   return (
     <>
-      <header className="site-nav fixed container max-w-7xl mx-auto top-0 z-50 w-full shadow-none transition-shadow duration-300">
-        <div className="flex items-center justify-between py-4 md:py-6 bg-background px-5 lg:px-14">
+      <nav
+        className={`site-nav container max-w-7xl mx-auto shadow-none transition-shadow duration-300 fixed w-full top-0 z-50  lg:pb-0 ${open && "bottom-0"}`}>
+        <div
+          className={`py-4 md:py-6 bg-background px-5 lg:px-14 max-w-7xl mx-auto shadow-12 flex justify-between items-start lg:items-center  rounded-2xl relative shadow-[inset_6px_6px_200px_gba(150,150,150,0.25), inset_-6px_-6px_20px_rgba(150,150,150,0.45)] ${
+            open ? "menu overflow-y-auto" : "h-22"
+          }`}>
           <div className="text-lg font-semibold text-gradient text-gradient-animated">
             chysomm
           </div>
 
-          <nav className="flex items-center gap-3 text-sm">
+          <div className="flex items-center gap-2 md:gap-3 text-sm">
             <div
               className={
                 open
-                  ? `w-full h-screen fixed top-12 left-0 flex flex-col items-center justify-center gap-8 bg-background md:static md:bg-transparent md:h-auto md:w-auto md:flex-row md:gap-6`
-                  : "hidden md:flex md:items-center md:gap-6"
+                  ? `lg:w-full flex items-center flex-col lg:h-auto lg:flex-row lg:justify-between lg:bg-transparent lg:p-0 z-10 transition duration-700 ease-in-out absolute top-20 left-5 right-5 bottom-5 lg:relative`
+                  : "hidden w-full lg:flex items-center justify-center flex-col lg:static lg:h-auto lg:flex-row lg:justify-between transition duration-500 ease-in-out subMenu"
               }>
-              <>
-                {NAV_LINKS.map(link => (
-                  <a
-                    key={link.href}
-                    href={link.href}
-                    className=" link-underline ">
-                    {link.label}
-                  </a>
+              <ul className="flex flex-col lg:flex-row lg:ml-10 lg:justify-center lg:items-center gap-8 lg:gap-7 lg:flex-1 text-left lg:text-center w-full lg:px-1 flex-1 py-6 lg:py-0">
+                {NAV_LINKS.map((link, i) => (
+                  <li
+                    key={i}
+                    onClick={() => setOpen(false)}
+                    className="link-underline text-base w-fit">
+                    <a href={link.href}>{link.label}</a>
+                  </li>
                 ))}{" "}
-                <ResumeButton />
-              </>
+                <ResumeButton className="mt-auto lg:mt-0" />
+              </ul>
             </div>
 
             {/* Theme toggle */}
@@ -102,9 +112,9 @@ export default function Nav() {
                 <MenuIcon width={18} height={18} aria-hidden />
               )}
             </button>
-          </nav>
+          </div>
         </div>
-      </header>
+      </nav>
     </>
   );
 }
